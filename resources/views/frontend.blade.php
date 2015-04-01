fc1283c249270053ea97b062e5b64f318888f002
Quan Do <thienkimlove@gmail.com>
2015-04-01 08:43:37 +0000

    aaa

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -31,11 +31,11 @@
         });
     };
 
-    (function(d, s, id){
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
+        if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=697301236951060&version=v2.3";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 </script>
