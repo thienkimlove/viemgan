06057c7adba39ca633822c77e844fb87e4700a42
Quan Do <quandm@smartosc.com>
2015-05-04 11:54:11 +0700

    a

--- resources/views/frontend/list.blade.php
+++ resources/views/frontend/list.blade.php
@@ -2,7 +2,7 @@
     @foreach ($posts as $post)
         <article class="item">
             <a href="{{url($post->slug.'.html')}}" title="{{$post->title}}">
-                <img src="{{url('files/images/200_'. $post->image)}}" width="220" height="130" alt="{{$post->title}}"/>
+                <img src="{{url('render/?p=' . $post->image . '&w=220&h=130')}}" alt="{{$post->title}}" />
             </a>
             <time class="time" datetime="{{$post->updated_at}}0">{{$post->updated_at}}</time>
             <h3>
