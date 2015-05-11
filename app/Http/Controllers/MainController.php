38f525b736110223cf0bc8ea6f6c8eea2d66aa8a
Quan Do <quandm@smartosc.com>
2015-05-11 11:00:10 +0700

    A

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -49,7 +49,7 @@ class MainController extends Controller
                     ->where('category_id', $category->id)
                     ->whereNotIn('id', $idArrays)
                     ->latest()
-                    ->take(6)
+                    ->take(9)
                     ->get();
             }
 
@@ -60,7 +60,7 @@ class MainController extends Controller
                     ->where('category_id', $category->id)
                     ->whereNotIn('id', $idArrays)
                     ->latest()
-                    ->take(9)
+                    ->take(6)
                     ->get();
 
             }
